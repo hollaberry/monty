c18f6302d28c39cded59b90448f0b662014c3228
hollaberry <you@example.com>
2021-12-12 05:37:12 -0800

    output

--- _getline.c
+++ _getline.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include "monty.h" 
+#include "monty.h"
 ssize_t _getdelim(char **buf, size_t *bufsiz, int delimiter, FILE *fp)
 {
 char *ptr, *eptr;
@@ -11,29 +11,31 @@ char *ptr, *eptr;
 
 if (*buf == NULL || *bufsiz == 0) {
 *bufsiz = BUFSIZ;
-if ((*buf = malloc(*bufsiz)) == NULL)
-return -1;
+*buf = malloc(*bufsiz);
+if (*buf == NULL)
+return (-1);
 }
 
 for (ptr = *buf, eptr = *buf + *bufsiz;;) {
 int c = fgetc(fp);
 if (c == -1) {
 if (feof(fp))
-return ptr == *buf ? -1 : ptr - *buf;
+return (ptr == *buf ? -1 : ptr - *buf);
 else
-return -1;
+return (-1);
 }
 *ptr++ = c;
 if (c == delimiter) {
 *ptr = '\0';
-return ptr - *buf;
+return (ptr - *buf);
 }
 if (ptr + 2 >= eptr) {
 char *nbuf;
 size_t nbufsiz = *bufsiz * 2;
 ssize_t d = ptr - *buf;
-if ((nbuf = realloc(*buf, nbufsiz)) == NULL)
-return -1;
+nbuf = realloc(*buf, nbufsiz);
+if ((nbuf == NULL)
+return (-1);
 *buf = nbuf;
 *bufsiz = nbufsiz;
 eptr = nbuf + nbufsiz;
@@ -44,5 +46,5 @@ ptr = nbuf + d;
 
 ssize_t _getline(char **buf, size_t *bufsiz, FILE *fp)
 {
-return getdelim(buf, bufsiz, '\n', fp);
+return (getdelim(buf, bufsiz, '\n', fp));
 }
